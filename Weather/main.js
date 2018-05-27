c203793cd3645ec7a2bbc669555da89aeb9a68c4
alpha87 <jianxun2004@gmail.com>
2018-05-27 11:33:08 +0800

    将today视图独立

--- Weather/main.js
+++ Weather/main.js
@@ -1,3 +1,10 @@
 var app = require('scripts/app');
+var todayApp = require('scripts/today');
 
-app.main()
+if ($app.env !== $env.today) {
+    $ui.loading("客官稍等片刻")
+    app.main()
+} else {
+    todayApp.main()
+}
+
