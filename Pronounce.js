be035ca45b5d16c92f2824fe488922f16b712047
alpha87 <jianxun2004@gmail.com>
2018-06-18 12:16:45 +0800

    增加错误判断

--- Pronounce.js
+++ Pronounce.js
@@ -1,12 +1,24 @@
 $input.text({
     type: $kbType.ascii,
-    placeholder: "输入单词",
+    placeholder: "输入需要发音的单词",
     text: $clipboard.text,
     handler: function (text) {
         $ui.loading("加载中")
-        $audio.play({
-            url: "https://ali.baicizhan.com/r/" + text.trim() + ".mp3"
+        $http.get({
+            url: "https://ali.baicizhan.com/r/" + text.trim() + ".mp3",
+            handler: function (resp) {
+                var data = resp.data
+                if (data.error) {
+                    $ui.toast(data.error)
+                    $ui.loading(false)
+                } else {
+                    $audio.play({
+                        url: "https://ali.baicizhan.com/r/" + text.trim() + ".mp3"
+                    })
+                    $ui.loading(false)
+                    $device.taptic(0)
+                }
+            }
         })
-        $ui.loading(false)
     }
 })
