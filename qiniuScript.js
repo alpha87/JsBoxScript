3c32ae27df827200ed8b45f1b52cd96e0e5159f5
Jianxun <jianxun2004@gmail.com>
2018-06-26 15:49:57 +0800

    Create qiniuScript.js

---
+++ qiniuScript.js
@@ -0,0 +1,51 @@
+String.prototype.urlsafe = function () {
+    return this.replace(/\+/g, "-").replace(/\//g, "_")
+}
+var CryptoJS = require("crypto-js")
+var AK = "WR1-2JkrICcpLvdw15LnuUbtTUeLBCDPHjPPRyF9"
+var SK = "1blyh54_uoulFjMPPAt2N-GEXDgwKH-4n1vJICgd"
+var bucket = "jsboxscript"
+var domain = "p9fwt88yw.bkt.clouddn.com"
+var deadline = Math.round(new Date().getTime() / 1000) + 1 * 3600
+var params = { scope: bucket, deadline: deadline }
+var policy = JSON.stringify(params)
+var policyEncoded = $text.base64Encode(policy).urlsafe()
+var sign = CryptoJS.HmacSHA1(policyEncoded, SK)
+var signEncoded = sign.toString(CryptoJS.enc.Base64).urlsafe()
+var token = AK + ":" + signEncoded + ":" + policyEncoded
+
+function pickImage() {
+    $photo.pick({
+        format: "image",
+        handler: function (resp) {
+            var image = resp.image
+            image ? upload(image) : null
+        }
+    })
+}
+
+function upload(image) {
+    $http.upload({
+        url: "http://up-z1.qiniu.com/",
+        form: {
+            token: token
+        },
+        files: [{
+            "image": image,
+            "name": "file",
+            "filename": "file"
+        }],
+        handler: function (resp) {
+            if (resp.data.key) {
+                var url = "http://" + domain + "/" + resp.data.key
+                $clipboard.text = url
+                $device.taptic(0)
+                $ui.toast("链接已复制到剪贴板")
+            } else {
+                $ui.toast(resp.data.error)
+            }
+        }
+    })
+}
+
+pickImage()
